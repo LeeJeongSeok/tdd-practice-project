da44782572bb632736b296727f3192fabcb0a790
LeeJeongSeok <ljs0429777@gmail.com>
2020-04-25 16:20:24 +0900

    2장 완료 Junit3 살짝 맛보기 코드 추가

---
+++ src/main/java/com/lee/practice/tdd/Account2.java
@@ -0,0 +1,20 @@
+package com.lee.practice.tdd;
+
+public class Account2 {
+
+    public Account2(int i) {
+
+    }
+
+    public int getBalance() {
+        return 0;
+    }
+
+    public void withDraw() {
+
+    }
+
+    public void deposit() {
+
+    }
+}
---
+++ src/test/java/com/lee/practice/tdd/Account2Test.java
@@ -0,0 +1,32 @@
+package com.lee.practice.tdd;
+
+import junit.framework.TestCase;
+
+import static org.assertj.core.api.Assertions.fail;
+
+public class Account2Test extends TestCase {
+
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    public void testAccount() {
+        fail("Not yet implmented");
+    }
+
+    public void testGetBalance() {
+        fail("Not yet implmented");
+    }
+
+    public void testWithDraw() {
+        fail("Not yet implmented");
+    }
+
+    public void testDeposit() {
+        fail("Not yet implmented");
+    }
+}
